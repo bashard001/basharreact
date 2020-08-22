84769ce623b8334e5f66ed1db8e6301c4a1fb4ed
unknown <bdaseh@gmail.com>
2020-08-21 17:46:09 -0700

    react

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Bg1 from "./components/Bg1"
 import List from "./components/List"
 import Area from "./components/Area"
 import Footer from "./components/Footer"
+import Totop from "./components/Totop"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Bg1 c1="bg3" c2="caption" c3="box" text="SCROLL UP"/>
       <Area text="Scroll up and down to really get the feeling of how Parallax Scrolling works." />
   <Bg1 c1="bg1" c2="caption" c3="box lastBox" text={<a className="projects" href="myprojects.html">Checkout my projects</a>}/>
+  <Totop />
   <Footer />
     </div>
   );
---
+++ src/components/Totop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./stylesheets/totop.css"
+
+
+function Totop (){
+    return(
+        <div id="goup"> 
+        <a class="totop"  href="#top">
+       <i class="far fa-arrow-alt-circle-up"></i>
+      </a></div>
+    )
+}
+
+export default Totop
