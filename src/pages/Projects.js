95f04c19b190f577b99c6b27fd7a75028d238cdc
bashard001 <bdaseh@gmail.com>
2020-09-21 23:05:17 -0700

    animating cards in my projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,123 +4,100 @@ import "../components/stylesheets/projects.css"
 
 function Project() {
 
-  let [toolHeight, setToolHeight] = useState(0)
-  // let [mouseIsOver, setMouseIsOver] = useState(true)
-
-
-
-
-  let cardAni = function () {
-    const toolTip = document.querySelector(".tooltips")
-    const boxff = document.querySelector(".boxA")
-    function frame() {
-      var boxHe = 402
-
-      if (toolHeight == 0) {
-
-
-        clearInterval(anima);
-
-      } else {
-
-
-        toolHeight--;
-        boxHe -= 2
-        toolTip.style.height = toolHeight + 'px';
-      }
-    }
-
-    let anima = setInterval(frame, 2)
-  }
-
-  const CardON = function () {
-    const toolTip = document.querySelector(".tooltips")
-    const boxff = document.querySelector(".boxA")
-
-    var boxH = 200
-    var boxM = 23
-    function frame() {
-
-      if (toolHeight == 200) {
-
-        clearInterval(animaON);
-      } else {
-        toolHeight++;
-        boxH += 2
-        boxM++
-        toolTip.style.height = toolHeight + 'px';
-
-      }
-    }
-
-    let animaON = setInterval(frame, 2)
-  }
 
   return (
     <div className="maincontainers">
-        <div className="pagetitle">
-          <h2 >My Projects</h2>
-          <p>(Web applications)</p>
-        </div>
+      <div className="pagetitle">
+        <h2 >My Projects</h2>
+        <p>(Web applications)</p>
+      </div>
       <div className="containers">
-      <div className="box a2">
-        <div className="captions">
-          <a href="https://bashard001.github.io/AstronomyQuiz" target="_blank"  className="ap">
-           
-           <span>Sample Quiz</span>
-       </a>
-       </div>
-      </div>
-      <div className="box a2">
-        <div className="captions">
-          <a href="https://bashard001.github.io/AstronomyQuiz" target="_blank"  className="ap">
-           
-           <span>Sample Quiz</span>
-       </a>
-       </div>
-      </div>
-      {/* <div className="tooltips"><p className="tool-text"> This application is built using 95% JavaScript <br /> highest score is save to localstorage</p></div> */}
-        {/* <div className="boxA " onMouseOver={CardON} onMouseLeave={cardAni}> <div className="captions a2" >
-          <a href="https://bashard001.github.io/AstronomyQuiz" target="_blank" >
-           
-              <span className="box">Sample Quiz</span>
+        <div className="box ">
+          <div className="a2">
+          <div className="captions ">
+            <a href="https://bashard001.github.io/AstronomyQuiz" target="_blank" className="ap">
 
-            <div className="tooltips"><p className="tool-text"> This application is built using 95% JavaScript <br /> highest score is save to localstorage</p></div>
-          </a></div> </div>
-        <div className="boxA">
-          <a href="https://bashard001.github.io/weather-dashboard" target="_blank" class="a3">
-            <div className="captions">
-              <span class="box">Check the Weather</span>
-            </div>
-          </a>
-        </div> */}
+              <span>Sample Quiz</span>
+            </a>
+          </div>
+          </div>
+          <div className="tooltips"><p className="tool-text"> This application is built using 95% JavaScript <br /> highest score is save to localstorage</p></div>
+        </div>
 
-        {/* <a href="https://bashard001.github.io/Day-Planner" target="_blank" class="boxA a4">
+
+        <div className="box " >
+          <div className="a3">
           <div className="captions">
-            <span class="box">Day Calender</span>
+            <a href="https://bashard001.github.io/weather-dashboard" target="_blank" className="ap">
+
+              <span>Check the Weather</span>
+            </a>
           </div>
-        </a>
-        <a href="https://jilienecircelli.github.io/sports-scene" target="_blank" class="boxA a5" >
-          <div class="captions">
-            <span class="box">Sports Events</span>
           </div>
-        </a>
-        <a href="https://bashard001.github.io/Password-Generator" target="_blank" class="boxA a1" >
-          <div class="captions">
-            <span class="box">Password Generator</span>
+          <div className="tooltips"><p className="tool-text"> For this app I used JQuery, bootstrap, Weather api<br/>and localstorage to save search history</p></div>
+
+        </div>
+       
+        <div className="box ">
+          <div className="a4">
+          <div className="captions ">
+            <a href="https://bashard001.github.io/Day-Planner" target="_blank" className="ap">
+
+              <span>Day Calender</span>
+            </a>
           </div>
-        </a>
-        <a href="https://employeedirectory360.herokuapp.com/" target="_blank" class="boxA a6" >
-          <div className="captions">
-            <span className="box">Employee Directory</span>
           </div>
-        </a>
-        <a href="#" target="" class="boxA a7 ">
-          <div className="captions">
-            <span className="box">Snake Game</span>
+          <div className="tooltips"><p className="tool-text"> This app shows my ability to build an application <br /> with the least amount of code to save size</p></div>
+        </div>
+
+        <div className="box ">
+          <div className="a5">
+          <div className="captions ">
+            <a href="https://jilienecircelli.github.io/sports-scene" target="_blank" className="ap">
+
+              <span>Sports Events</span>
+            </a>
           </div>
-        </a> */}
-      </div> 
+          </div>
+          <div className="tooltips"><p className="tool-text"> Using multiple APIs to find local sporting events <br/> and the ability to buy a ticket</p></div>
+        </div>
+
+        <div className="box ">
+          <div className="a1">
+          <div className="captions ">
+            <a href="https://bashard001.github.io/Password-Generator" target="_blank" className="ap">
+
+              <span>Password Generator</span>
+            </a>
+          </div>
+          </div>
+          <div className="tooltips"><p className="tool-text"> writing complex algorithm <br/> to generate a Password</p></div>
+        </div>
+
+        <div className="box ">
+          <div className="a6">
+          <div className="captions ">
+            <a href="https://employeedirectory360.herokuapp.com/" target="_blank" className="ap">
+
+              <span>Employee Directory</span>
+            </a>
+          </div>
+          </div>
+          <div className="tooltips"><p className="tool-text"> React application</p></div>
+        </div>
+        <div className="box ">
+          <div className="">
+          <div className="captions ">
+            <a href="https://employeedirectory360.herokuapp.com/" target="_blank" className="ap">
+
+              <span>Snake Game</span>
+            </a>
+          </div>
+          </div>
+          <div className="tooltips"><p className="tool-text">Snack Game Coming <br/> Soon</p></div>
+        </div>
+
+      </div>
 
     </div>
   )
