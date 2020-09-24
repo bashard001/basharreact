d66b06848e2141984330d4fcf48f9bf523bf6bab
bashard001 <bdaseh@gmail.com>
2020-09-23 18:44:20 -0700

    working on the about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,33 +4,35 @@ import "../components/stylesheets/about.css"
 
 
 
-function About () {
-  return(
+function About() {
+  return (
     <div className="maincontainer" id="top">
 
-    <div className="container">
+
 
       <div className="mainbox card">
         <h2 className="pagetitle">About Me</h2>
 
         <div className="cardcontent">
+          <div className="aitem">
+            <div className="imgab"></div>
+            <h3 style={{ fontfFamily: 'Charmonman', fontSize: "20px", color: "red" }}>Full Stack Web Developer
+          </h3><br />
+            <strong>Bashar Daseh</strong>
+            <br />
+            <br />
+            <i style={{ display: "inline" }} className="fa fa-phone"> &#32; </i>
+            <a className="contactLink"
+              href="tel:8587175630">858-717-5630</a><br />
+            <i style={{ display: "inline" }} className="fa fa-envelope-o"> &#32; </i>
+            <a className="contactLink"
+              href="mailto:bdaseh@gmail">bdaseh@gmail.com</a><br />
+            <i style={{ display: "inline", textDecoration: "none" }} className="fa fa-github"> &#32; </i><a
+              class="contactLink" href="https://github.com/bashard001" target="_blank">
+              github.com/bashard001</a>
+          </div>
 
-          <div className="imgab"></div>
-          <h3 style={{fontfFamily: 'Charmonman',fontSize: "20px", color: "red"}}>Full Stack Web Developer
-          </h3><br/>
-          <strong>Bashar Daseh</strong>
-          <br/>
-          <br/>
-          <i style={{display: "inline"}} className="fa fa-phone"> &#32; </i>
-          <a className="contactLink" 
-            href="tel:8587175630">858-717-5630</a><br/>
-          <i style={{display: "inline"}} className="fa fa-envelope-o"> &#32; </i>
-          <a className="contactLink" 
-            href="mailto:bdaseh@gmail">bdaseh@gmail.com</a><br/>
-          <i style={{display: "inline", textDecoration: "none"}} className="fa fa-github"> &#32; </i><a
-          class="contactLink" href="https://github.com/bashard001" target="_blank">
-            github.com/bashard001</a>
-          <h3>Development Tools, Languages and Frameworks:</h3>
+<div className="aitem2"> <h3>Development Tools, Languages and Frameworks:</h3>
           <ul class="skillList 1">
             <li className="aboutlist">Java</li><li className="aboutlist">Wordpress</li>
             <li className="aboutlist">Javascript</li>
@@ -41,10 +43,11 @@ function About () {
             <li className="aboutlist">springMVC</li>
             <li className="aboutlist">MySQL and MongoDB </li>
             <li className="aboutlist">Scrach</li>
-            <li className="aboutlist">MERN Stack <br/>(MongoDB, Express.js, React.js, Node.js).</li>
+            <li className="aboutlist">MERN Stack <br />(MongoDB, Express.js, React.js, Node.js).</li>
 
-          </ul>
-          <h3>Education:</h3>
+          </ul></div>
+
+          <div className="aitem3"><h3>Education:</h3>
           <ul >
             <li className="aboutlist">University of San Diego California (UCSD). 2018-2020</li>
             <p> BS in Computer Science</p>
@@ -52,14 +55,18 @@ function About () {
             <p>Computer Science Education</p>
             <li className="aboutlist">San diego Mesa College. 2012-2016</li>
             <p> Associate of Science in Biology & Allied health</p>
-          </ul>
-        </div>
+          </ul></div>
+         
+
+
+          
+       
 
 
 
 
 
-        <div class="box2 ">
+        <div class="aitem4">
           <h3>About: </h3>
 
           <p class="text">
@@ -67,20 +74,20 @@ function About () {
             web-based applications, backend development and data engineering that meet the highest standards for user
             experience, web design, usability, and speed. proven experience developing consumer-focused web applications
             for desktop and mobile using MERN, HTML, CSS, JQuery, SQL, JavaScript and node.js.</p>
-     <p class="text">
+          <p class="text">
             Proven experience developing consumer-focused web applications using HTML, CSS, JQuery, PHP , JavaScript,
             Node.js, MySQL and more... Building applications for desktop, and mobile devices that meet the highest
-            standards for user experience, web design, usability, and speed.</p> 
+            standards for user experience, web design, usability, and speed.</p>
 
 
-      <p class="text">Solution-driven professional excelling in highly collaborative work environment, finding
-            solutions to challenges and focused on customer satisfaction. Solution-driven professional excelling in
-            highly collaborative work environment, finding solutions to challenges and focused on customer satisfaction.
-            development (including creating servers, processing data,
-            source control, issue tracking, functional feedback, etc...). I have advanced skills in Javascript and Java
+          <p class="text">Solution-driven professional excelling in highly collaborative work environment, finding
+          solutions to challenges and focused on customer satisfaction. Solution-driven professional excelling in
+          highly collaborative work environment, finding solutions to challenges and focused on customer satisfaction.
+          development (including creating servers, processing data,
+          source control, issue tracking, functional feedback, etc...). I have advanced skills in Javascript and Java
             for programing various applications (mobile apps, desktop apps, games, and more) </p>
-
-          <h4 >Business & Finance: </h4>
+</div>
+<div className="aitem5"><h4 >Business & Finance: </h4>
           <p >
             Working in the financial markets as a technical analyst and web Developer for about four years, generating
             HTML, PDF files using node js and MySQL to Analyze, draft create edit and prepare financial plans and data
@@ -88,11 +95,14 @@ function About () {
             attention and continual analysis of economic trends to determine appropriate asset allocation according to
             strict standards.
 
-          </p>
+          </p></div>
+          
+        
+
         </div>
       </div>
+
     </div>
-  </div>
   )
 
 }
