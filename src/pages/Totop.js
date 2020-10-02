c3b450a37903d32d44628b26c36569195d085206
unknown <bdaseh@gmail.com>
2020-10-02 15:54:52 -0700

    Enhanced the functionality of the scroll to top button using JS

--- src/pages/Totop.js
+++ src/pages/Totop.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "../components/stylesheets/totop.css"
 
 
+
+
 function Totop (){
+     
+
+
+
+const scrollFunction = () => {
+    var myButton = document.querySelector("#goup")
+
+    if (document.body.scrollTop > 110 || document.documentElement.scrollTop > 110) {
+        myButton.style.opacity = 1;
+    } else {
+       
+        myButton.style.opacity = 0;
+    }
+  }
+  function toTopFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  useEffect(()=>{
+    
+      window.addEventListener("scroll", scrollFunction)
+  },[])
+
     return(
-        <div id="goup"> 
-        <a class="totop"  href="#top">
+        <div id="goup" onClick={toTopFunction}> 
+        <a class="totop"  >
        <i class="far fa-arrow-alt-circle-up"></i>
       </a></div>
     )
