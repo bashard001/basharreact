dc726478291c9ee541acfa716d1972ba79f8793c
unknown <bdaseh@gmail.com>
2020-10-01 21:48:01 -0700

    php

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react"
 import "../components/stylesheets/contact.css"
-import "./contactform.php"
-
+require("../pages/contactform.php")
 
 function Contact(props) {
 
@@ -12,11 +11,34 @@ function Contact(props) {
     })
     
 
-
+const resetForm = () => {
+    setContact({
+        name: "",
+        email: "",
+        message: ""
+    })
+}
 
   const  handleSubmit = event => {
     event.preventDefault();
-    console.log(contact);
+
+ fetch('http://localhost:3000/send', {
+      method: "POST",
+      body: JSON.stringify(contact),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    }).then(
+    (response) => (response.json())
+      ).then((response)=> {
+    if (response.status === 'success') {
+      alert("Message Sent."); 
+      resetForm()
+    } else if(response.status === 'fail') {
+      alert("Message failed to send.")
+    }
+  })
     }
 
     const onNameChange = event => {
@@ -49,8 +71,8 @@ function Contact(props) {
                         <form id="contact-form" onSubmit={handleSubmit.bind(this)} method="POST">
                             <label for="fname" >First Name</label>
                             <input className="form-control" value={contact.name}  onChange={onNameChange.bind(this)} type="text" id="fname" name="firstname" placeholder="Your name..." required />
-                            {/* <label for="lname">Last Name</label>
-                            <input type="text" id="lname" name="lastname" placeholder="your last name..." className="form-control" /> */}
+                             <label for="lname">Last Name</label>
+                            <input type="text" id="lname" name="lastname" placeholder="your last name..." className="form-control" /> 
                             <label for="email-address" >Email address</label>
                             <input className="form-control" value={contact.email} onChange={onEmailChange.bind(this)} type="email" id="email-address" name="emailaddress" placeholder=" enter your email" required />
                             <label for="message" >message</label>
