2e829bbfd73cd1995c00844f83ae9d354d2d5552
unknown <bdaseh@gmail.com>
2021-06-28 14:03:53 -0700

    updated content

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -17,7 +17,7 @@ function Main (){
       <Area text="Scroll up and down to really get the feeling of Parallax Scrolling Animation" />
       <Bg1 c1="bg3" c2="caption" c3="box" text="SCROLL UP"/>
       <Area text="Scroll up and down to really get the feeling of how Parallax Scrolling works." />
-  <Bg1 c1="bg1" c2="caption" c3="box lastBox" text={<Link to={'/projects'} className="projects"><span className="largeC">Checkout my projects</span><span className="smallC">My projects</span></Link>}/>
+  <Bg1 c1="bg4" c2="caption" c3="box lastBox" text={<Link to={'/projects'} className="projects"><span className="largeC">Checkout my projects</span><span className="smallC">My projects</span></Link>}/>
 
  
     </div>
