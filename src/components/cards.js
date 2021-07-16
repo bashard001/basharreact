fdb67ade341501c9362ce194b99d365a630202c4
unknown <bdaseh@gmail.com>
2021-07-16 13:11:36 -0700

    added styling to the card component

--- src/components/cards.js
+++ src/components/cards.js
@@ -3,50 +3,49 @@ import { Images } from "../assets/img";
 
 class Card extends React.Component {
 
-constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = props
-}
+  }
 
 
-    render(){
-      
-      var image
-      const { project } = this.state
-      console.log(project)
- switch (project.pImage) {
-              case "clothing": image = Images.clothing
-                
-                break;
-                case "password": image = Images.password
-              break
-              case "quiz": image = Images.quiz
-              break
-              case "sports": image = Images.sports
-              break
-              case "calendar": image = Images.calender
-              break
-              case "weather": image = Images.weather
-              break
-              default:
-                break;
-            }
+  render() {
 
-        return(
-            <div className="box ">
-                <div className="a2" style={{ backgroundImage: `url(${image})` }}>
-                  <div className="captions ">
-                    <a rel="noopener noreferrer" target="_blank" className="ap">
-
-                      <span>{project.pName}</span>
-                    </a>
-                  </div>
-                </div>
-                <div className="tooltips"><p className="tool-text">{project.pDisc}</p></div>
-              </div>
-        )
+    var image
+    const { project } = this.state
+    console.log(project)
+    switch (project.pImage) {
+      case "clothing": image = Images.clothing
+        break;
+      case "password": image = Images.password
+        break
+      case "quiz": image = Images.quiz
+        break
+      case "sports": image = Images.sports
+        break
+      case "calendar": image = Images.calender
+        break
+      case "weather": image = Images.weather
+        break
+      default:
+        break;
     }
+
+    return (
+      <div className="box ">
+        <div className="a" style={{ backgroundImage: `url(${image})` }}>
+          <div className="captions ">
+            <a href={project.pUrl} rel="noopener noreferrer" target="_blank" className="ap">
+
+              <span>{project.pName}</span>
+            </a>
+          </div>
+        </div>
+        <div className="tooltips"><p className="tool-text">{project.pDisc}</p></div>
+      </div>
+    )
+  }
 }
 
 export default Card
