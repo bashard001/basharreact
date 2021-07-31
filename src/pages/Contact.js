197686a5450ab209cef7b4f082cac09aca01d78e
unknown <bdaseh@gmail.com>
2021-07-30 19:15:55 -0700

    updating contact page info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -73,13 +73,13 @@ function Contact() {
                     <div className="column">
                         <form id="contact-form"  >
                             <label htmlFor="fname" >First Name</label>
-                            <input className="form-control" value={contact.name} onChange={onNameChange.bind(this)} type="text" id="fname" name="firstname" placeholder="Your name..." required />
+                            <input className="form-control" value={contact.name} onChange={onNameChange.bind(this)} type="text" id="fname" name="firstname" placeholder="First Name" required />
                             <label htmlFor="lname">Last Name</label>
-                            <input type="text" id="lname" value={contact.lName} onChange={onlNameChange.bind(this)} name="lastname" placeholder="your last name..." className="form-control" />
+                            <input type="text" id="lname" value={contact.lName} onChange={onlNameChange.bind(this)} name="lastname" placeholder="Last Name" className="form-control" />
                             <label htmlFor="email-address" >Email address</label>
-                            <input className="form-control" value={contact.email} onChange={onEmailChange.bind(this)} type="email" id="email-address" name="emailaddress" placeholder=" enter your email" required />
+                            <input className="form-control" value={contact.email} onChange={onEmailChange.bind(this)} type="email" id="email-address" name="emailaddress" placeholder=" Enter your email" required />
                             <label htmlFor="message" >message</label>
-                            <textarea className="form-control" value={contact.message} onChange={onMessageChange.bind(this)} id="message" name="message" placeholder="leave a message"
+                            <textarea className="form-control" value={contact.message} onChange={onMessageChange.bind(this)} id="message" name="message" placeholder="Write a message"
                                 style={{ height: '160px' }} required></textarea>
                             <div id="formSub">
                                 <input id="contactSub" onClick={handleSubmit.bind(this)} type="submit" name="submit" value="submit" /></div>
