b5e9e910c43a65768317306966f234773b20cd1e
unknown <bdaseh@gmail.com>
2021-08-29 18:10:53 -0700

    card data update

--- src/pages/projectsPage/project.data.js
+++ src/pages/projectsPage/project.data.js
@@ -10,14 +10,14 @@ const projectsData = [ {
     pUrl: "https://astronomy-qui.web.app",
     pImage: "quiz",
     pName: "Sample Quiz",
-    pDisc: "This application is built using 95% JavaScript <br /> highest score is save to localstorage"
+    pDisc: "This application is built using 95% JavaScript  highest score is save to localstorage"
   },
    {
     id: 3,
     pUrl: "https://bashard001.github.io/weather-dashboard",
     pImage: "weather",
     pName: "Check the weather", 
-    pDisc: "For this app I used JQuery, bootstrap, Weather api<br />and localstorage to save search history"
+    pDisc: "For this app I used JQuery, bootstrap, Weather api and localstorage to save search history"
   
   },
   {
@@ -25,21 +25,21 @@ const projectsData = [ {
     pUrl: "https://bashard001.github.io/Day-Planner",
     pImage: "calendar",
     pName: "Day Calender",
-    pDisc: "This app shows my ability to build an application <br /> with the least amount of code to save size"
+    pDisc: "This app shows my ability to build an application with the least amount of code to save size"
   },
   {
     id: 5,
     pUrl: "https://jilienecircelli.github.io/sports-scene",
     pImage: "sports",
     pName: "Sports Events",
-    pDisc: "Using multiple APIs to find local sporting events <br /> and the ability to buy a ticket"
+    pDisc: "Using multiple APIs to find local sporting events and the ability to buy a ticket"
   },
   {
     id: 6,
     pUrl: "https://bashard001.github.io/Password-Generator",
     pImage: "password",
     pName: "Password Generator",
-    pDisc: "writing complex algorithm <br /> to generate a Password"
+    pDisc: "writing complex algorithm to generate a Password"
   }]
 
   export default projectsData
