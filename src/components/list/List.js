0cb9777f39dc00579c1257bb31a953df87c02a23
unknown <bdaseh@gmail.com>
2021-09-02 19:33:04 -0700

    info

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -9,12 +9,12 @@ function List() {
       <h3>Web Applications</h3>
 
       <ul >
-        <li>Apply best accepted “social coding” practices. (Including Test-driven development, source control, bug tracking,
+        <li>Building Web Applications using the latest technologies and best practices. (Including Test-driven development, source control, bug tracking,
           functional feedback, etc.).</li>
         <li>Build full stack web Apps from scratch. And Deploy static and dynamic web apps to the cloud.</li>
         <li>Implement complex logical conditions to meet an objective</li>
         <li>Implement APIs into my apps using Fetch, cloud functions and AJAX communication </li>
-        <li> Create and consume RESTful APIs utilizing JSON as a data format and REST APIs </li>
+        <li> Create and consume RESTful APIs utilizing JSON as a data format </li>
       </ul>
     </div>
   
