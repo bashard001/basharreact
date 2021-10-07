a93432be17d3e51d1ef6e33d8fc623651c1723b4
bashard001 <bdaseh@gmail.com>
2021-10-07 00:14:56 -0700

    down wave line in svg and css

--- src/pages/homepage/Main.js
+++ src/pages/homepage/Main.js
@@ -19,7 +19,7 @@ function Main() {
             <Bg1 c1="bg1 wave" c2="caption firstcaption" c3="firstbox" c4="top" text="Full Stack Web Engineer" />
            
             <List />
-            <Bg1 c1="bg2" c2="caption" c3="box" text="Apply Animations" />
+            <Bg1 c1="bg2 down" c2="caption" c3="box" text="Apply Animations" />
             <Area text="Scroll up and down to really get the feeling of Parallax Scrolling Animation" />
             <Bg1 c1="bg3" c2="caption" c3="box" text="SCROLL" />
             <Area text="Scroll up and down to really get the feeling of how Parallax Scrolling works." />
