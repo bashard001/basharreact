71b61d88623d9f31b2147993ba32bf31935966d1
bashard001 <bdaseh@gmail.com>
2021-10-22 14:47:11 -0700

    header background color white when header transparent

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,6 +1,5 @@
 import React, {useEffect} from "react"
 import { Link } from "react-router-dom"
-
 import "./nav.scss"
 
 
@@ -10,21 +9,21 @@ function Nav() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
+
 function scroll (){
     window.addEventListener("scroll", function(){
         var header = document.querySelector(".header")
-        console.log(window.scrollY)
-        header.classList.toggle("sticky", window.scrollY >= 70)
+        console.log(Math.floor(window.scrollY))
+        header.classList.toggle("sticky", Math.floor(window.scrollY)  > 0)
     })
 }
-
 
 useEffect(()=>{
     scroll()
 })
     return (
         <nav className="header">
-          
+     
                 <section id="left">
                     <Link to={'/'} onClick={scrollToTop}>
                         <span id="a">Bashar Daseh</span><span id="b">B D</span>
