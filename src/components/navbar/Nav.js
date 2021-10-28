abf6f5c0827b798be57fd755b8d63e545e848c3c
bashard001 <bdaseh@gmail.com>
2021-10-27 22:13:59 -0700

    css underline

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import { Link } from "react-router-dom"
 
 import "./nav.scss"
 
 
 function Nav() {
+
+    let timeOut = useRef("")
+  
+
 
     function scrollToTop() {
         document.body.scrollTop = 0;
@@ -18,10 +22,34 @@ function Nav() {
         })
     }
 
+    const handleHover = (e) => {
+        clearTimeout(timeOut.current)
+        
+        let el = e.target.querySelector(".underline")
+        
+        if (el) {
+            el.style.opacity = "1"
+            el.style.transform = "translateX(0)"
+        }
+    }
+    
+    const hoverOut = (e) => {
+        let el = e.target.querySelector(".underline")
+        if (el) {
+            el.style.transform = "translateX(101%)"
+            timeOut.current = setTimeout(() => {
+                el.style.opacity = "0"
+                el.style.transform = "translateX(-101%)"
+            }, 380)
+        }
+    }
 
     useEffect(() => {
         scroll()
     })
+
+
+
     return (
         <nav className="header">
 
@@ -31,18 +59,31 @@ function Nav() {
                 </Link>
             </section>
             <section id="right">
-                <Link to={'/projects'} onClick={scrollToTop}> <div className="navlink"><i className="fab fa-connectdevelop"> </i>
-                <div className="navlink_contain">
-                    <p>Projects</p>
-                    <div className="underline"></div>
+                <Link to={'/projects'} onMouseOver={handleHover} onMouseLeave={hoverOut} onClick={scrollToTop} >
+                    <div className="navlink"  ><i className="fab fa-connectdevelop"> </i>
+                        <div className="navlink_contain">
+                            <p>Projects</p>
+                            <div style={{transform: "translateX(-101%)"}} className="underline"></div>
+                        </div>
                     </div>
-                    
-                </div>
-                   
                 </Link>
-                <Link to={'/about'} onClick={scrollToTop}><div className="navlink"><i className="far fa-user-circle"></i><p>About</p></div></Link>
+                <Link to={'/about'} onMouseOver={handleHover} onMouseLeave={hoverOut} onClick={scrollToTop}>
+                    <div className="navlink">
+                        <i className="far fa-user-circle"></i>
+                        <div className="navlink_contain">
+                            <p>About</p>
+                            <div className="underline"></div>
+                        </div>
+                    </div>
+                </Link>
 
-                <Link to={'/contact'} onClick={scrollToTop}><div className="navlink"> <i className="fas fa-id-card-alt"> </i> <p> Contact</p></div>
+                <Link to={'/contact'} onMouseOver={handleHover} onMouseLeave={hoverOut} onClick={scrollToTop}><div className="navlink">
+                    <i className="fas fa-id-card-alt"> </i>
+                    <div className="navlink_contain">
+                        <p>Contact</p>
+                        <div className="underline"></div>
+                    </div>
+                </div>
                 </Link>
 
             </section>
