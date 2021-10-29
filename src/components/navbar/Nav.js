de315ca1514217dd57cef496db64f0a551872e62
bashard001 <bdaseh@gmail.com>
2021-10-28 23:42:32 -0700

    nav

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -22,30 +22,46 @@ function Nav() {
     }
 
     const handleHover = (e) => {
+        e.stopPropagation()
         clearTimeout(timeOut.current)
-
+        
+        let el2 = e.target.querySelector(".newline")
         let el = e.target.querySelector(".underline")
-
-
-        el.style.opacity = "1"
-        el.style.transform = "translateX(0)"
-
+        console.log(e.target)
+        el2.style.width = "100%"
+        el2.style.right = "0"
+        //    el.style.transition= "transform .6s"
+        // el.style.opacity = "1"
+        // el.style.transform = "translateX(0)" 
+       
     }
+
+    
 
     const hoverOut = (e) => {
+        e.stopPropagation()
         let el = e.target.querySelector(".underline")
-        el.style.transform = "translateX(101%)"
-        timeOut.current = setTimeout(() => {
-            el.style.opacity = "0"
-            el.style.transform = "translateX(-101%)"
-        }, 380)
+        let el2 = e.target.querySelector(".newline")
+        el2.style.right = "0"
+        el2.style.left = ""
+        el2.style.width = "0%"
+        // console.log(el)
+        // el.style.transform = "translateX(101%)"
+        // timeOut.current = setTimeout(() => {
+        //     el.style.transition= "transform 0s"
+        //     el.style.opacity = "0"
+        //     el.style.transform = "translateX(-101%)"
+        // }, 380)
 
     }
+    useEffect(()=>{
+        document.querySelectorAll(".linkNav").forEach(el => el.addEventListener("mouseenter", handleHover))
+        document.querySelectorAll(".linkNav").forEach(el => el.addEventListener("mouseleave", hoverOut))
+    },[])
 
     useEffect(() => {
         scroll()
     })
-
 
     return (
         <nav className="header">
@@ -55,8 +71,9 @@ function Nav() {
                     <span id="a">Bashar Daseh</span><span id="b">B D</span>
                 </Link>
             </section>
+
             <section id="right">
-                <Link to={'/projects'} onMouseEnter={handleHover} onMouseLeave={hoverOut} onClick={scrollToTop} >
+                <Link to={'/projects'} className="linkNav" onClick={scrollToTop} >
                     <div className="navlink"  ><i className="fab fa-connectdevelop"> </i>
                         <div className="navlink_contain">
                             <p>Projects</p>
@@ -64,7 +81,8 @@ function Nav() {
                         </div>
                     </div>
                 </Link>
-                <Link to={'/about'} onMouseEnter={handleHover} onMouseLeave={hoverOut} onClick={scrollToTop}>
+
+                <Link to={'/about'} className="linkNav" onClick={scrollToTop}>
                     <div className="navlink">
                         <i className="far fa-user-circle"></i>
                         <div className="navlink_contain">
@@ -74,18 +92,18 @@ function Nav() {
                     </div>
                 </Link>
 
-                <Link to={'/contact'} onMouseEnter={handleHover} onMouseLeave={hoverOut} onClick={scrollToTop}>
+                <Link to={'/contact'} className="linkNav" onClick={scrollToTop}>
                     <div className="navlink">
                         <i className="fas fa-id-card-alt"> </i>
                         <div className="navlink_contain">
                             <p>Contact</p>
+                            <div className="newline"></div>
                             <div className="underline"></div>
+                            
                         </div>
                     </div>
                 </Link>
-
             </section>
-
         </nav>
     )
 }
