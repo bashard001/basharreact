9b992a561126367afc894dbfbfbdb8bcd0d33ba8
bashard001 <bdaseh@gmail.com>
2023-07-31 20:46:31 -0700

    book

--- src/components/icon-templates/IconLink.jsx
+++ src/components/icon-templates/IconLink.jsx
@@ -1,28 +1,45 @@
+/** @jsxImportSource @emotion/react */
 import { useState } from "react";
+import { css } from "@emotion/react";
 
-const IconLink = ({ Icon, href, color = "black", fontSize = 260, margin = 4, shouldHover = false }) => {
-    const [isHover, setIsHover] = useState(false);
+const IconLink = ({
+  Icon,
+  href,
+  color = "black",
+  fontSize = 260,
+  shouldHover = false,
+}) => {
+  const [isHover, setIsHover] = useState(false);
 
-    const iconStyles = {
-        display: "inline-flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: isHover ? `${fontSize * 1.2}px` : `${fontSize}px`,
-        transition: "font-size 0.2s ease",
-        color: color,
-    };
+  const iconStyles = css`
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    font-size: ${isHover ? fontSize * 1.2 + "px" : fontSize + "px"};
+    transition: font-size 0.2s ease;
+    color: ${color};
+  `;
 
-    return (
-        <a href={href} target="_blank" rel="noopener noreferrer" style={{ display: "flex", justifyContent: "center", margin: `${margin}px`}}>
-            <div
-                onMouseEnter={() => shouldHover ? setIsHover(true) : null}
-                onMouseLeave={() => shouldHover ? setIsHover(false) : null}
-                style={iconStyles}
-            >
-                <Icon />
-            </div>
-        </a>
-    );
-}
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="social media noreferrer"
+      css={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      `}
+    >
+      <div
+        onMouseEnter={() => (shouldHover ? setIsHover(true) : null)}
+        onMouseLeave={() => (shouldHover ? setIsHover(false) : null)}
+        css={iconStyles}
+      >
+        <Icon />
+      </div>
+    </a>
+  );
+};
 
 export default IconLink;
